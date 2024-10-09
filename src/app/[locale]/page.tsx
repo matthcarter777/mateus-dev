d7dc58d5f75d47444ecb984520736b6fdf2306ae
mateus <mateushbsb@gmail.com>
2024-10-09 13:48:32 -0300

    feat: config tailwind

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const t = useTranslations('HomePage');
 
   return (  
-    <div>
+    <div className="bg-gray-900">
       <h1>{t('title')}</h1>
       <Link href="/about">{t('about')}</Link>
     </div>
