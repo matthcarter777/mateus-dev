a33e9b7cbb5db43282a2fa42956c0f9a8868db5d
mateus <mateushbsb@gmail.com>
2024-10-18 13:57:56 -0300

    fix: text color

--- src/app/components/profile/index.tsx
+++ src/app/components/profile/index.tsx
@@ -6,8 +6,8 @@ export default function Profile() {
   
   return (
     <div className='px-8'>
-      <h2 className='text-4xl font-bold mb-4'>{t('name')}</h2>
-      <h4 className='text-2xl font-medium mb-4'>{t('title')}</h4>
+      <h2 className='text-4xl font-bold mb-4 text-white'>{t('name')}</h2>
+      <h4 className='text-2xl font-medium mb-4 text-white'>{t('title')}</h4>
       <p className='text-slate-500'>{t('description')}</p>
     </div>
   )
