f1b240ce40c81dde1c225eed2c224054a6d540c4
mateus <mateushbsb@gmail.com>
2024-10-18 13:45:19 -0300

    fix: logo animation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,24 +1,26 @@
 import { Metadata } from 'next';
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 import '../globals.css'
 
 export const metadata: Metadata = {
   title: "Mateus Silva Dev",
-  description: "Mateus Silva Dev",
+  description: "Mateus Silva Dev",  
 };
 
 export default async function RootLayout({
   children,
-  params: {locale}
+  params: { locale }
 }: Readonly<{
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }>) {
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
+      <head>
+      </head>
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,24 +1,52 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { FaSpinner } from 'react-icons/fa'; // Importa o ícone de carregamento do react-icons
 import SocialMedia from '../components/socialMedia';
 import Navbar from '../components/navbar';
 import Profile from '../components/profile';
 import Summary from '../components/summary';
 import Experience from '../components/experiences';
+import Image from 'next/image';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  }, []);
 
   return (
-    <div className="bg-gray-900 w-screen h-screen">
-      <div className="grid grid-cols-2 h-full">
-        <div className="h-full max-h-80 box-content p-20 sticky top-0">
-          <Profile />
-          <Navbar />
-          <SocialMedia />
+    <div className="bg-gray-900 w-screen h-screen flex items-center justify-center">
+      {loading ? (
+        <div className="flex flex-col items-center">
+          <div className="animate-pulse">
+            <Image 
+              src="/images/logo.svg" 
+              alt="Logo" 
+              width={150} 
+              height={150} 
+              className="transition-transform duration-500 transform scale-100 hover:scale-110" // Efeito de escalonamento
+            />
+          </div>
         </div>
-        <div className="box-content p-20 overflow-y-auto">
-          <Summary />
-          <Experience />
+      ) : (
+        <div className="grid grid-cols-2 h-full">
+          <div className="h-full max-h-80 box-content p-20 sticky top-0">
+            <Profile />
+            <Navbar />
+            <SocialMedia />
+          </div>
+          <div className="box-content p-20 overflow-y-auto">
+            <Summary />
+            <Experience />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
